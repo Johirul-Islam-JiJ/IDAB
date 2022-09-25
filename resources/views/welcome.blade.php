4dad2f26645e6d8815c15c35760f16693632a124
unknown <you@example.com>
2022-09-25 17:30:32 +0600

    Nav Bar updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,24 +21,38 @@
         </style>
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+        {{-- <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0"> --}}
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                        @endif
-                    @endif
-                </div>
-            @endif
 
             <div id="app">
+                 {{-- @if (Route::has('login'))
+                        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                            @auth
+                            <a
+                                href="{{ url('/home') }}"
+                                class="text-sm text-gray-700 underline"
+                            >
+                                Home
+                            </a>
+                            @else
+                            <a
+                                href="{{ route('login') }}"
+                                class="text-sm text-gray-700 underline"
+                            >
+                                Login
+                            </a>
+                            @if (Route::has('register'))
+                            <a
+                                href="{{ route('register') }}"
+                                class="ml-4 text-sm text-gray-700 underline"
+                            >
+                                Register
+                            </a>
+                            @endif @endif
+                        </div>
+                        @endif --}}
             </div>
             <script src="js/app.js"></script>
-        </div>
+        {{-- </div> --}}
     </body>
 </html>
