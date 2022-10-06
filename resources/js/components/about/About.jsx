f286cf6f80ab1fcde659aa57707474762a58a636
unknown <you@example.com>
2022-10-06 14:27:44 +0600

    About Idab Complete

--- resources/js/components/about/About.jsx
+++ resources/js/components/about/About.jsx
@@ -1,142 +1,181 @@
+// import React, { useState } from "react";
 import React from "react";
 import "./about.css";
-import { RiCheckboxMultipleFill } from "react-icons/ri";
 
-const About = () => {
+
+const Aobut = () => {
     return (
-        <section id="about">
-            <h5> Skill i have achieved</h5>
-            <h2>My Experience</h2>
-
-            <div className="container experience_container">
-                <div className="experience_frontend">
-                    <h3>Fronend Development</h3>
-                    <div className="experience_content">
-                        <article className="experience_details">
-                            <RiCheckboxMultipleFill className="experience_detals-icon" />
-                            <div>
-                                <h4>HTML</h4>
-                                <small className="text-light">
-                                    Experienced
-                                </small>
-                            </div>
-                        </article>
-
-                        <article className="experience_details">
-                            <RiCheckboxMultipleFill className="experience_detals-icon" />
-                            <div>
-                                <h4>CSS</h4>
-                                <small className="text-light">
-                                    Experienced
-                                </small>
-                            </div>
-                        </article>
-
-                        <article className="experience_details">
-                            <RiCheckboxMultipleFill className="experience_detals-icon" />
-                            <div>
-                                <h4>Java Script</h4>
-                                <small className="text-light">
-                                    Intermediate
-                                </small>
-                            </div>
-                        </article>
-
-                        <article className="experience_details">
-                            <RiCheckboxMultipleFill className="experience_detals-icon" />
-                            <div>
-                                <h4>Bootsrap</h4>
-                                <small className="text-light">
-                                    Intermediate
-                                </small>
-                            </div>
-                        </article>
-
-                        <article className="experience_details">
-                            <RiCheckboxMultipleFill className="experience_detals-icon" />
-                            <div>
-                                <h4>React.js</h4>
-                                <small className="text-light">
-                                    Experienced
-                                </small>
-                            </div>
-                        </article>
-
-                        <article className="experience_details">
-                            <RiCheckboxMultipleFill className="experience_detals-icon" />
-                            <div>
-                                <h4>Tainwind</h4>
-                                <small className="text-light">
-                                    Experienced
-                                </small>
-                            </div>
-                        </article>
-                    </div>
-                </div>
-
-                <div className="experience_Backend">
-                    {/*=============== End Of Front=================
-          ===================Backend Start================ */}
-
-                    <h3>Backend Development</h3>
-                    <div className="experience_content">
-                        <article className="experience_details">
-                            <RiCheckboxMultipleFill className="experience_detals-icon" />
-                            <div>
-                                <h4>Node.js</h4>
-                                <small className="text-light">
-                                    Intermediate
-                                </small>
-                            </div>
-                        </article>
-
-                        <article className="experience_details">
-                            <RiCheckboxMultipleFill className="experience_detals-icon" />
-                            <div>
-                                <h4>django</h4>
-                                <small className="text-light">
-                                    Experienced
-                                </small>
-                            </div>
-                        </article>
-
-                        <article className="experience_details">
-                            <RiCheckboxMultipleFill className="experience_detals-icon" />
-                            <div>
-                                <h4>Laravel</h4>
-                                <small className="text-light">
-                                    Intermediate
-                                </small>
-                            </div>
-                        </article>
-
-                        <article className="experience_details">
-                            <RiCheckboxMultipleFill className="experience_detals-icon" />
-                            <div>
-                                <h4>My SQL</h4>
-                                <small className="text-light">
-                                    Intermediate
-                                </small>
-                            </div>
-                        </article>
-
-                        <article className="experience_details">
-                            <RiCheckboxMultipleFill className="experience_detals-icon" />
-                            <div>
-                                <h4>Firebase</h4>
-                                <small className="text-light">
-                                    Experienced
-                                </small>
-                            </div>
-                        </article>
-                    </div>
-                </div>
-
-                {/* <div className='experience_backend'>
-        </div> */}
+        <div className="container">
+            <div className="welcome_massage">
+                <h2>Welcome massage from President</h2>
+                <p>
+                    IDAB (Interior designer’s association Of Bangladesh) founded
+                    2019 with some young and energetic Bangladeshi Interior
+                    Designers to represent the Interior designers & interior
+                    design industry as a whole in Bangladesh.
+                    <br />
+                    <br />
+                    From 2020 IDAB start organizing all Bangladeshi Interior
+                    designer, architects to bring them under one umbrella. This
+                    process is ongoing. When you join the association of
+                    Interior Designers, you become part of national and
+                    international network of interior designers. We are trying
+                    to make IDAB as a professional organization who work for
+                    interior designers and architects.
+                    <br />
+                    <br />
+                    As a non-profit organization governed by law, the
+                    Association has not only to protect the standards of
+                    professionalism for Interior Designers, but to be able to
+                    resolve industry issues. It has a serious duty to promote
+                    the creativity and professionalism of the Bangladesh
+                    Interior Design Industry to the world.
+                    <br />
+                    <br />
+                    We are here to make the Connectivity, Recognition,
+                    Knowledge, Networking, Networking, Marketing, Accreditation,
+                    Overseas Network Bridging, Publicity and Contribute to the
+                    Bangladeshi Society. We want to make a beautiful Bangladesh
+                    together. “UNITED” we can make bonding and off course a
+                    dream Bangladesh.
+                    <br />
+                    <br />
+                    We have lots of dream. But we need to be united, we need to
+                    come under one umbrella.
+                    <br />
+                    Bangladesh, as we all know is a tiny small green dot on the
+                    world map, but every day we become a leader in different
+                    sector, now a days IDAB wants to work for become a leader
+                    for world interior design society.
+                    <br />
+                    <br />
+                    IDAB will work with other professional bodies of different
+                    foreign countries to share and exchange knowledge for the
+                    betterment of our beloved profession. We wish one day we
+                    can.
+                    <br />
+                    <br />
+                    BE UNITED, this is the only way to become a pleasant
+                    Interior designer.
+                    <br />
+                    Thank you and best regards. Shafiul Islam Convener Interior
+                    Designer’s Association of Bangladesh.
+                </p>
             </div>
-        </section>
+
+
+            <div className="history">
+                <h2>History</h2>
+                <p>
+                    The Interior Designers Association of Bangladesh (IDAB) is
+                    the national institute representing the interior design
+                    profession in Bangladesh.
+                    <br />
+                    <br />
+                    In the early 1900’s, the term “Interior Decorator” was first
+                    used in America. Most decorators at the time had no academic
+                    credentials, but the best of them had a combination of good
+                    taste, common senses, and natural talent to interpret and
+                    address the issues, such as scale and proportion. Elsie de
+                    Wolfe became the first Interior Decorator to be given a
+                    design “commission.” In 1913, Elsie de Wolfe published the
+                    first interior design book, “The House in Good Taste.”
+                    Dorothy Draper was the first documented commercial Interior
+                    Decorator, establishing her design firm in 1923.
+                    <br />
+                    <br />
+                    The term “Interior Designer” was coined in the 1930’s, by a
+                    magazine called “Interior Design and Decoration.” That
+                    magazine was not printed between 1943 and 1952, but a
+                    competing magazine, “Interiors,” insisted on using only the
+                    term “interior designer,” not “decorator.” At this time
+                    design schools also recognized the work not only of
+                    designers but also of architects and engineers. “Interiors”
+                    published more contract work than residential work. When
+                    “Interior Design and Decoration” magazine resumed
+                    publication, it dropped the term “and Decoration” from its
+                    name In 1995, Berger Bangladesh Limited started an Interior
+                    design School in Bangladesh with Exterior-Interior Pvt
+                    Limited (Ex-In, India) which follow a some several interior
+                    design school in Bangladesh. Shanto-Mariam University of
+                    Creative Technology introduced Interior Architecture
+                    education in Bangladesh through establishing Interior
+                    Architecture Department in the year 2003. Since then, the
+                    department has been serving the nation with excellence in
+                    Interior Architecture education. Now there are more than
+                    3000 Interior designers who work with their creativity to
+                    make a beautiful Bangladesh every day. For This number of
+                    Interior designers founded IDAB with their passion,
+                    dedication in 2020 which is now under the process for
+                    Bangladesh Govt. registration.
+                </p>
+            </div>
+            <div className="vision">
+                <h2>Vision and mission of IDAB</h2>
+                <p>
+                    The objectives of the association shall be:
+                    <br />
+                    <br />
+                    1. To advance the standards of Interior Design Practice,
+                    along with materials, products, and associated services
+                    related to the field of Interior Design and Implementation
+                    particularly in Bangladesh.
+                    <br />
+                    <br />
+                    2. To help establish closer contact as co-operation among
+                    Designers, Technologists, Managers, and Trainers working in
+                    Research Organizations, Industries and Training Institutes
+                    in the fields of Interior design as outlined in the article.
+                    <br />
+                    <br />
+                    3. To identify the problems in the Interior design
+                    Industries, Interior design Research Organizations, and
+                    Interior design Training Institutes and to recommend their
+                    solutions to the proper authority.
+                    <br />
+                    <br />
+                    4. To help improve the professional efficiency of Designers,
+                    Technologists, Managers and Trainers engaged in Interior
+                    design Research, Production, or Training activities.
+                    <br />
+                    <br />
+                    5. To help create awareness about Interior design among the
+                    general public by arranging lectures, seminars, workshops,
+                    exhibitions, etc.
+                    <br />
+                    <br />
+                    6. To publish newsletters, periodicals, and journals on
+                    Interior design, Furniture Design, and Building materials.
+                    <br />
+                    <br />
+                    7. To send delegates to seminars, workshops, conferences,
+                    etc. at home and abroad if and when invited to attend such
+                    events.
+                    <br />
+                    <br />
+                    8. To establish contact with other Interior design
+                    associations and societies of the world and with other
+                    research institutes, industries, and training institutes of
+                    the world dealing with Interior design.
+                    <br />
+                    <br />
+                    9. To procure leaflets, newsletters, periodicals, journals,
+                    books, etc. on Interior design from home and abroad and
+                    establish a library on Interior design with such literature.
+                    <br />
+                    <br />
+                    10. To offer an award for best design on Residence Design,
+                    Commercial Design, landscaping.
+                    <br />
+                    <br />
+                    11. To offer, if and when possible, grants/scholarships for
+                    Interior design and to students learning Interior design.
+                    <br />
+                    <br />
+                </p>
+            </div>
+        </div>
     );
 };
 
-export default About;
+export default Aobut;
